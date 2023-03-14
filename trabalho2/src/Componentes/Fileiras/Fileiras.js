053dfc64e04ea36d646a21ca344f1c5ae5c6fb44
Thiago Albino <thiagoalbino2000@hotmail.com>
2023-03-14 16:56:10 -0300

    Terminando as fileiras

--- trabalho2/src/Componentes/Fileiras/Fileiras.js
+++ trabalho2/src/Componentes/Fileiras/Fileiras.js
@@ -1,39 +1,40 @@
 import React, { useState } from "react";
 import './Fileiras.css';
-import { SlArrowLeft } from 'react-icons/sl';
-import { SlArrowRight } from 'react-icons/sl';
+import { IoIosArrowDropleftCircle } from 'react-icons/io';
+import { IoIosArrowDroprightCircle } from 'react-icons/io';
+import { BsInfoCircle } from 'react-icons/bs';
 
 
 export default ({ title, items }) => {
 
     const [scrollX, setScrollX] = useState(0)
     const handleLeftArrow = () => {
-        let x = scrollX + Math.round(window.innerWidth /2);
-        if(x>0){
+        let x = scrollX + Math.round(window.innerWidth / 3);
+        if (x > 0) {
             x = 0;
         }
         setScrollX(x);
     }
-    
+
     const handleRightArrow = () => {
-        let x = scrollX - Math.round(window.innerWidth /2);
-        let listW = items.results.length * 150;
-        if((window.innerWidth - listW) > x){
-            x = (window.innerWidth - listW) - 60;
+        let x = scrollX - Math.round(window.innerWidth / 3);
+        let listW = items.results.length * 165;
+        if (x < ((window.innerWidth) - listW)) {
+            x = 0;
         }
         setScrollX(x);
-    }
+    };
 
     return (
 
         <div className='fileiras'>
             <h2>{title}</h2>
             <div className='scroll-esquerda' onClick={handleLeftArrow}>
-                <SlArrowLeft style={{ fontSize: 50 }} />
+                <IoIosArrowDropleftCircle style={{ fontSize: 50 }} />
             </div>
 
             <div className='scroll-direita' onClick={handleRightArrow}>
-                <SlArrowRight style={{ fontSize: 50 }} />
+                <IoIosArrowDroprightCircle style={{ fontSize: 50 }} />
             </div>
 
 
@@ -42,13 +43,16 @@ export default ({ title, items }) => {
 
                 {/* Area que faz scroll nos filmes */}
                 <div className="lista-fileiras"
-                style = {{marginLeft: scrollX,
-                    width: items.results.length *150
-                }}
+                    style={{
+                        marginLeft: scrollX,
+                        width: items.results.length * 150
+                    }}
                 >
                     {items.results.length > 0 && items.results.map((item, key) => (
                         <div key={key} className="item-fileiras">
                             <img src={`https://image.tmdb.org/t/p/w300/${item.poster_path}`} alt={item.original_title} />
+                            <div className="nome-item-fileiras">{item.title}</div>
+                            <button href={`/descricao/${item.overview}`} className="overview-fileiras"><BsInfoCircle style={{ fontSize: 30 }} /></button>
                         </div>
                     ))}
                 </div>
@@ -56,3 +60,4 @@ export default ({ title, items }) => {
         </div>
     );
 }
+
